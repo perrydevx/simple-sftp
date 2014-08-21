e4503a0295b0a75867404f0d4a1bdeafc9456857
perry <perry@mbp17.local>
2014-08-21 17:53:15 +0800

    add license, etc.

--- SimpleSftpGrailsPlugin.groovy
+++ SimpleSftpGrailsPlugin.groovy
@@ -16,54 +16,12 @@ class SimpleSftpGrailsPlugin {
         Simple way to connect and perform basic file actions to an SFTP server. Allows application to upload, download, delete, rename, create directory to an SFTP Server with one method call.
     '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/simple-sftp"
+    def documentation = "https://github.com/perrydevx/simple-sftp"
 
-    // Extra (optional) plugin metadata
+    def license = "APACHE"
 
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def issueManagement = [ system: "GitHub", url: "https://github.com/perrydevx/simple-sftp/issues" ]
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    def scm = [ url: "https://github.com/perrydevx/simple-sftp" ]
+    
 }
