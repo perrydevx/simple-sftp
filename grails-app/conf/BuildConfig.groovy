163dd7bddf06cc7968cb4e84043cf16837499a43
perry <perry@mbp17.local>
2014-08-25 19:49:25 +0800

    add jcraft dependecies in BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -37,6 +37,8 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
         // runtime 'mysql:mysql-connector-java:5.1.27'
+
+        compile "com.jcraft:jsch:0.1.49"
     }
 
     plugins {
